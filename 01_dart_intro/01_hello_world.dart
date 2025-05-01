190b98c56352a321dff95baf03962be9a2c5b697
Efrain Guzman <GSefrainAmail.com>
2025-05-01 15:04:21 -0500

    commit rama main

---
+++ 01_dart_intro/01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+  //var myName = "Alejo";
+  //late final myName;
+  //String myName = "Alejo";
+
+  const myName = "Alejo";
+
+  print("Hola $myName");
+  print("Hola ${myName.toUpperCase}");
+  print("Hola ${1 + 1}");
+}
